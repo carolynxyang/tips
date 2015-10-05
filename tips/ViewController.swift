d37af51cbcc3c9c1852268b97b701ebf7482e076
Carolyn Yang <cyang6@l-sb8c1jufft-m.local>
2015-10-05 15:48:37 -0700

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Carolyn Yang - Vendor on 10/5/15.
+//  Copyright © 2015 Carolyn Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
